4356a285d6b177ab9c9defccc6543dd41b370fef
xansiety <ferando543@outlook.com>
2023-01-28 14:09:56 -0600

    Filtros en todos

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,24 @@ const initialStateTodos = [
 const App = () => {
     const [todos, setTodos] = useState(initialStateTodos);
 
+    //filtros
+    const [filter, setFilter] = useState("all");
+
+    const changeFilter = (filter) => setFilter(filter);
+
+    const filteredTodos = () => {
+        switch (filter) {
+            case "all":
+                return todos;
+            case "completed":
+                return todos.filter((todo) => todo.completed);
+            case "active":
+                return todos.filter((todo) => !todo.completed);
+            default:
+                return todos;
+        }
+    };
+
     const createTodo = (title) => {
         setTodos([
             ...todos,
@@ -39,14 +57,31 @@ const App = () => {
         setTodos(todos.filter((todo) => todo.id !== id));
     };
 
+    const computedTodosLeft = todos.filter((todo) => !todo.completed).length;
+    const computedTodosCompleted = todos.filter(
+        (todo) => todo.completed
+    ).length;
+
+    const clearCompleted = () => {
+        setTodos(todos.filter((todo) => !todo.completed));
+    };
+
     return (
         <div className="min-h-screen bg-gray-200 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
             <Header />
             <main className="container mx-auto mt-8 px-4">
                 <TodoCreate createTodo={createTodo} />
-                <TodoList todos={todos} removeTodo={removeTodo} updateTitle={updateTitle} />
-                <TodoComputed />
-                <TodoFilter />
+                <TodoList
+                    todos={filteredTodos()}
+                    removeTodo={removeTodo}
+                    updateTitle={updateTitle}
+                />
+                <TodoComputed
+                    computedTodosLeft={computedTodosLeft}
+                    computedTodosCompleted={computedTodosCompleted}
+                    clearCompleted={clearCompleted}
+                />
+                <TodoFilter changeFilter={changeFilter} filter={filter} />
             </main>
             <Footer />
         </div>
--- src/components/TodoComputed.jsx
+++ src/components/TodoComputed.jsx
@@ -1,8 +1,14 @@
-const TodoComputed = () => {
+const TodoComputed = ({ computedTodosLeft, clearCompleted, computedTodosCompleted }) => {
     return (
         <section className="flex justify-between rounded-b-md bg-white py-4 px-4">
-            <span className="text-gray-500">5 Items left</span>
-            <button className="text-gray-500">Clear All</button>
+            <span className="text-gray-500">
+                {computedTodosLeft} Items left
+            </span>
+            {computedTodosCompleted  > 0 && (
+                <button onClick={clearCompleted} className="text-gray-500">
+                    Clear All completed
+                </button>
+            )}
         </section>
     );
 };
