7c1366fcc3c7963ea499327b80b1f9ddbeadc928
xansiety <ferando543@outlook.com>
2023-01-28 15:46:26 -0600

    DarkMode por preferencias del usuario o mediante prefrencias del sistema operativo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import IconMoon from "./icons/IconMoon";
+import IconSun from "./icons/IconSun";
+
+const initialTheme = localStorage.getItem("theme") === "dark";
 
 const Header = () => {
+    const [darkMode, setDarkMode] = useState(initialTheme);
+
+    // Esta función se ejecuta cada vez que cambia el valor de darkMode, es decir estar pendiente de ese valor
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+        }
+    }, [darkMode]);
+
     return (
         <header className="container mx-auto px-4 pt-8">
             <div className="flex justify-between">
                 <h1 className="text-3xl font-semibold uppercase tracking-[0.3em] text-white">
                     Todo
                 </h1>
-                <button>
-                    <IconMoon color="#fff" />
+                <button onClick={() => setDarkMode(!darkMode)}>
+                    {darkMode ? <IconSun /> : <IconMoon />}
                 </button>
             </div>
         </header>
